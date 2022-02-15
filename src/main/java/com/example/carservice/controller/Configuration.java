9cccc669e381bdb5759c1f11a2352585a510f31a
grizbi <hary20022222@gmail.com>
2022-02-15 12:44:28 +0100

    mend
    Modified entity classes, fixed bug with generating table scheme, added CommandLineRunner class for filling the car table with default data, created controller clas and populated it with /cars endpoint

--- src/main/java/com/example/carservice/controller/Configuration.java
+++ src/main/java/com/example/carservice/controller/Configuration.java
@@ -6,18 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Configuration {
-
-
+    
     @GetMapping("/cars")
     public String getCars(){
 
-
         return "Cars";
     }
-
-
-
-
-
 
 }
